85b918e20c21fb639e608c5b21172412472fce86
unknown <malakathennakoon@gmail.com>
2022-07-17 15:03:24 +0530

    first

---
+++ nginx.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-west-2"
+  access_key = "AKIAXVT6HIXSARKTIFXU"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-098e42ae54c764c35"
+    instance_type = "t2.micro"
+    key_name = "ec2_key"
+
+connection {
+  type     = "ssh"
+  user     = "ec2-user"
+  private_key = file("./ec2_key.pem")
+  host     = self.public_ip
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo amazon-linux-extras install -y nginx1",
+    "sudo systemctl start nginx"
+  ]
+}
+}
